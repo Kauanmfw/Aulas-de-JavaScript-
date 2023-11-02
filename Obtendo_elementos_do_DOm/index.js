883c94e3998210c28b04a2e9dafb21931078471c
Kauanmfw <Kauanmfw@outlook.com>
2023-11-01 23:08:38 -0300

    Aulas_javascript
    ElementosDom

---
+++ Obtendo_elementos_do_DOm/index.js
@@ -0,0 +1,10 @@
+function show() {
+  const contactlist = document.getElementById("contact-list");
+  console.log(contactlist);
+
+  const listElements = document.getElementsByTagName("li");
+  console.log(listElements);
+
+  const contactInputs = document.getElementsByClassName("contact-input");
+  console.log(contactInputs);
+}
